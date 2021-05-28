783ed92055c87b8b728b4a212afed36f664b265a
fatou mbathie <fatou.mbathie@eyone.net>
2021-05-28 14:11:03 +0100

    Gestion user et livre

---
+++ src/Entity/Livre.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LivreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LivreRepository::class)
+ */
+class Livre
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $maisonedition;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $auteur;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $statutlivre;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $disponibilite;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $datedentree;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $category;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getMaisonedition(): ?string
+    {
+        return $this->maisonedition;
+    }
+
+    public function setMaisonedition(string $maisonedition): self
+    {
+        $this->maisonedition = $maisonedition;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getStatutlivre(): ?int
+    {
+        return $this->statutlivre;
+    }
+
+    public function setStatutlivre(int $statutlivre): self
+    {
+        $this->statutlivre = $statutlivre;
+
+        return $this;
+    }
+
+    public function getDisponibilite(): ?bool
+    {
+        return $this->disponibilite;
+    }
+
+    public function setDisponibilite(bool $disponibilite): self
+    {
+        $this->disponibilite = $disponibilite;
+
+        return $this;
+    }
+
+    public function getDatedentree(): ?\DateTimeInterface
+    {
+        return $this->datedentree;
+    }
+
+    public function setDatedentree(\DateTimeInterface $datedentree): self
+    {
+        $this->datedentree = $datedentree;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
---
+++ src/Form/LivreType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Livre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LivreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('maisonedition')
+            ->add('auteur')
+            ->add('statutlivre')
+            ->add('disponibilite')
+            ->add('datedentree')
+            ->add('category')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}
